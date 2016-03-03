d80e43ec6f79912b36d37cc65cd379032ac28501
ivan.datsiv <ivan_datsiv@mail.ru>
2016-03-03 18:33:33 +0200

    Lesson_js12-13 created the methods.

--- GoIT/markup/home/lesson_js12-13/js/script.js
+++ GoIT/markup/home/lesson_js12-13/js/script.js
@@ -1,29 +1,59 @@
 /**
  * Created by Vania on 16.12.2015.
  */
+var body = document.body;
+var title = createTitle();
+var form = createForm();
+var test = createTest();
 
-var title = document.createElement('h1');
-title.innerHTML = "Тест по програмировинию";
-title.className = "title";
-document.body.appendChild(title);
 
-var form = document.createElement('form');
-form.setAttribute('action', 'answers');
-document.body.appendChild(form);
+addElements(body, title);
+addElements(body, form);
+addElements(form, test);
 
-var questions = document.createElement('ul');
-questions.className = "questions";
-form.appendChild(questions);
+createQuestions(test, 3, 3);
 
-for(var i = 1; i < 4; i++){
+var button = createButton();
+addElements(test, button);
 
-    var h3 = document.createElement('h3');
-    h3.innerHTML =+i +". Вопрос №" +i;
-    h3.className = 'questions_title';
-    var li = document.createElement('li');
-    li.appendChild(h3);
 
-    for(var j = 1; j < 4; j++) {
+function createTitle() {
+    var title = document.createElement('h1');
+    title.innerHTML = "Тест по програмировинию";
+    title.className = "title";
+    return title;
+}
+
+function createForm() {
+    var form = document.createElement('form');
+    form.setAttribute('action', 'answers');
+    return form;
+}
+
+function createTest() {
+    var test = document.createElement('ul');
+    test.className = "test";
+    return test;
+}
+
+function createQuestions(test1, quests, variants) {
+
+    for(var i = 1; i <= quests; i++){
+
+        var h3 = document.createElement('h3');
+        h3.innerHTML =+i +". Вопрос №" +i;
+        h3.className = 'questions_title';
+        var li = document.createElement('li');
+        li.appendChild(h3);
+
+        lis = createVariants(li, variants);
+
+        test1.appendChild(lis);
+    }
+}
+
+function createVariants(lis, variants) {
+    for (var j = 1; j <= variants; j++) {
 
         var li2 = document.createElement('li');
         li2.className = 'questions_item';
@@ -32,18 +62,23 @@ for(var i = 1; i < 4; i++){
         inputCheckbox.setAttribute("type", "checkbox");
 
         var spanText = document.createElement('span');
-        spanText.innerHTML =" Вариант ответа №" +j;
+        spanText.innerHTML = " Вариант ответа №" + j;
 
         li2.appendChild(inputCheckbox);
         li2.appendChild(spanText);
-        li.appendChild(li2);
+        lis.appendChild(li2);
     }
 
-    questions.appendChild(li);
+    return lis;
 }
 
-var submit = document.createElement('button');
-submit.setAttribute("type", "submit");
-submit.innerHTML = "Проверить мои результаты";
-submit.className = "bt_submit";
-questions.appendChild(submit);
+function createButton(){
+    var submit = document.createElement('button');
+    submit.setAttribute("type", "submit");
+    submit.innerHTML = "Проверить мои результаты";
+    submit.className = "bt_submit";
+}
+
+function addElements(parrent, child) {
+    parrent.appendChild(child);
+}
