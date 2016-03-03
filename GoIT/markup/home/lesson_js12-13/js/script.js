19f8caf519726c0e4f7d0537e0a9217d50b1d178
ivan.datsiv <ivan_datsiv@mail.ru>
2016-03-03 14:38:39 +0200

    Lesson_js12-13 deleted style in js file.

--- GoIT/markup/home/lesson_js12-13/js/script.js
+++ GoIT/markup/home/lesson_js12-13/js/script.js
@@ -1,15 +1,10 @@
 /**
  * Created by Vania on 16.12.2015.
  */
-document.body.style.margin = '0';
-document.body.style.padding = '0';
-document.body.style.fontFamily = 'sans-serif';
 
 var title = document.createElement('h1');
 title.innerHTML = "Тест по програмировинию";
-title.style.fontSize = '22px';
-title.style.fontWeight = 'normal';
-title.style.textAlign = 'center';
+title.className = "title";
 document.body.appendChild(title);
 
 var form = document.createElement('form');
@@ -18,28 +13,22 @@ document.body.appendChild(form);
 
 var questions = document.createElement('ul');
 questions.className = "questions";
-questions.style.listStyle = 'none';
 form.appendChild(questions);
 
 for(var i = 1; i < 4; i++){
 
     var h3 = document.createElement('h3');
     h3.innerHTML =+i +". Вопрос №" +i;
-    h3.style.fontSize = '22px';
-    h3.style.fontWeight = 'normal';
-    h3.style.marginBottom = '8px';
-
+    h3.className = 'questions_title';
     var li = document.createElement('li');
     li.appendChild(h3);
 
     for(var j = 1; j < 4; j++) {
 
         var li2 = document.createElement('li');
-        li2.style.marginLeft = '20px';
+        li2.className = 'questions_item';
 
         var inputCheckbox = document.createElement('input');
-        inputCheckbox.style.backgroundColor = 'grey';
-        inputCheckbox.style.border = '2px solid black';
         inputCheckbox.setAttribute("type", "checkbox");
 
         var spanText = document.createElement('span');
@@ -56,9 +45,5 @@ for(var i = 1; i < 4; i++){
 var submit = document.createElement('button');
 submit.setAttribute("type", "submit");
 submit.innerHTML = "Проверить мои результаты";
-submit.style.margin = '36px auto 0';
-submit.style.display = 'block';
-submit.style.backgroundColor = '#cfe2f3';
-submit.style.border = '2px solid black';
-submit.style.padding = '12px 36px';
+submit.className = "bt_submit";
 questions.appendChild(submit);
