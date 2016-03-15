59aabad9e3bace541a2aded26ffd069a653a7256
ivan.datsiv <ivan_datsiv@mail.ru>
2016-03-15 13:52:48 +0200

    Lesson_js14-15

--- GoIT/markup/home/lesson_js14-15/js/script.js
+++ GoIT/markup/home/lesson_js14-15/js/script.js
@@ -2,91 +2,68 @@
  * Created by ivan.datsiv on 3/14/2016.
  */
 
-var min = document.getElementById("min");
-var sec = document.getElementById("sec");
-var mili = document.getElementById("mili");
+var time = document.getElementById("time");
+var start = document.getElementById("start");
+var clear = document.getElementById("clear");
 var interval;
 
-
-var start = document.getElementById("start");
-var pause = document.getElementById("pause");
-var cont = document.getElementById("cont");
-var clear = document.getElementById("clear");
-
 start.addEventListener('click', handlerStart);
-pause.addEventListener('click', handlerPause);
-cont.addEventListener('click', handlerCont);
 clear.addEventListener('click', handlerStop);
 
 function handlerStart() {
     interval = setInterval(write, 5);
-    if (start.className == "button_show"){
-        changeStartToPause();
+    if (start.firstChild.nodeValue == "Start") {
+        changeFirstButton("Pause", handlerStart, handlerPause);
     }
 }
 
-function handlerPause (){
+function handlerPause() {
     clearInterval(interval);
-    changePauseToCont();
+    changeFirstButton("Cont", handlerPause, handlerCont);
 }
 
-function handlerCont (){
+function handlerCont() {
     handlerStart();
-    changeContToPause();
+    changeFirstButton("Pause", handlerCont, handlerPause);
 }
 
-function handlerStop (){
+function handlerStop() {
     clearInterval(interval);
-    mili.innerHTML = "000";
-    sec.innerHTML = "00";
-    min.innerHTML = "00";
-    changeButtonToStart();
+    time.innerHTML = "00 : 00 : 000";
+    (time.firstChild.nodeValue == "Pause") ? (changeFirstButton("Start", handlerPause, handlerStart)) : (changeFirstButton("Start", handlerCont, handlerStart));
 }
 
-function write () {
-    var ms = parseInt(mili.firstChild.nodeValue);
-    var s = parseInt(sec.firstChild.nodeValue);
-    var m = parseInt(min.firstChild.nodeValue);
-    ms +=5;
-    if (ms >= 999){
+function changeFirstButton(innerHTML, pre, next) {
+    start.innerHTML = innerHTML;
+    start.removeEventListener('click', pre);
+    start.addEventListener('click', next);
+}
+
+function write() {
+    var arrTime = time.firstChild.nodeValue.split(" : ");
+    var ms = parseInt(arrTime[2]);
+    var s = parseInt(arrTime[1]);
+    var m = parseInt(arrTime[0]);
+
+    ms += 5;
+    if (ms >= 999) {
         ms = 0;
         s += 1;
         if (s >= 59) {
             s = 0;
             m += 1;
-            min.innerHTML = m;
+            (m < 10) ? arrTime[0] = "0" + m : arrTime[0] = m;
         }
-        sec.innerHTML = s;
+        (s < 10) ? arrTime[1] = "0" + s : arrTime[1] = s;
     }
-    mili.innerHTML = ms;
-}
 
-function changeStartToPause(){
-    start.classList.add("button_hide");
-    start.classList.remove("button_show");
-    pause.classList.add("button_show");
-    pause.classList.remove("button_hide");
-}
+    if (ms < 10) {
+        arrTime[2] = "00" + ms;
+    } else if (ms >= 10 && ms < 100) {
+        arrTime[2] = "0" + ms;
+    } else {
+        arrTime[2] = ms;
+    }
 
-
-function changePauseToCont () {
-    pause.classList.add("button_hide");
-    pause.classList.remove("button_show");
-    cont.classList.add("button_show");
-    cont.classList.remove("button_hide");
-}
-
-function changeContToPause (){
-    cont.classList.add("button_hide");
-    cont.classList.remove("button_show");
-    pause.classList.add("button_show");
-    pause.classList.remove("button_hide");
-}
-function changeButtonToStart (){
-    start.classList.add("button_show");
-    start.classList.remove("button_hide");
-    pause.classList.add("button_hide");
-    pause.classList.remove("button_show");
-    cont.classList.add("button_hide");
-    cont.classList.remove("button_show");
+    time.innerHTML = arrTime.join(" : ");
 }
