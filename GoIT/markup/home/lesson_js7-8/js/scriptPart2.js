096ca503b35ae6ce7403283d51c52a075cf2a624
ivan.datsiv <ivan_datsiv@mail.ru>
2016-03-21 19:16:38 +0200

    Lesson_js7-8 part 2

---
+++ GoIT/markup/home/lesson_js7-8/js/scriptPart2.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ivan.datsiv on 3/21/2016.
+ */
+
+$(function () {
+    var tooltips = $("[title]").tooltip({
+        position: {
+            my: "left top",
+            at: "right+5 top-5"
+        }
+    })
+});
