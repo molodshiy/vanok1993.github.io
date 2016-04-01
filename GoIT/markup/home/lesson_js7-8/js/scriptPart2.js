04a4ed682dc92772bf204230a5813cf554e07bd4
ivan.datsiv <ivan_datsiv@mail.ru>
2016-04-01 17:41:04 +0300

    Lesson_js7-8 part 2

--- GoIT/markup/home/lesson_js7-8/js/scriptPart2.js
+++ GoIT/markup/home/lesson_js7-8/js/scriptPart2.js
@@ -2,11 +2,19 @@
  * Created by ivan.datsiv on 3/21/2016.
  */
 
-$(function () {
-    var tooltips = $("[title]").tooltip({
-        position: {
-            my: "left top",
-            at: "right+5 top-5"
-        }
-    })
+$(function(){
+    $("form input").hover(function() {
+        $(".t1").hide();
+        $(".t2").hide();
+        $(".t3").hide();
+
+
+        /*$(this).find("title").animate({opacity: "show", top: "-75"}, "slow");
+        var hoverText = $(this).attr("title");
+        hoverText.addClass("title1");
+        $(this).find("title").text(hoverText);
+    }, function() {
+        $(this).find("title").animate({opacity: "hide", top: "-85"}, "fast");*/
+    });
+
 });
