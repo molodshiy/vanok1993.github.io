e885854936c869c745834b14295805bee46c5b65
ivan.datsiv <ivan_datsiv@mail.ru>
2016-04-07 18:33:11 +0300

    lesson_js7-8 added var.

--- GoIT/markup/home/lesson_js7-8/js/script.js
+++ GoIT/markup/home/lesson_js7-8/js/script.js
@@ -3,14 +3,15 @@
  */
 
 $(function() {
+    var tabs = $('ul.tabs li');
 
-    $('ul.tabs li').click(function(){
+    tabs.click(function(){
         var thisClass = this.className.slice(0,2);
         $('div.t1').hide();
         $('div.t2').hide();
         $('div.t3').hide();
         $('div.' + thisClass).show();
-        $('ul.tabs li').removeClass('tab-current');
+        tabs.find('.tab-current').removeClass('tab-current');
         $(this).addClass('tab-current');
     });
 
