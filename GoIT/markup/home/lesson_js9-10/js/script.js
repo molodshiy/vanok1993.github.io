2a758c83d73703ffac4036235b32fa8793af3001
ivan.datsiv <ivan_datsiv@mail.ru>
2016-04-13 19:13:22 +0300

    lesson_js9-10 cont...

--- GoIT/markup/home/lesson_js9-10/js/script.js
+++ GoIT/markup/home/lesson_js9-10/js/script.js
@@ -2,43 +2,58 @@
  * Created by ivan.datsiv on 4/11/2016.
  */
 
-$(function(){
-    $("#default-usage-select").selectbox().bind('change', function () {
+$(function () {
+    /*$("#default-usage-select").selectbox().bind('change', function () {
         $('<div>Value of #default-usage-select changed to: ' + $(this).val() + '</div>').appendTo('#demo-default-usage .demoTarget').fadeOut(5000, function () {
             $(this).remove();
         });
-    });
+    });*/
 
-        $(".niceCheck").mousedown(
-        function() {
+    $(".niceCheck").mousedown(
+        function () {
             changeCheck($(this));
         });
 
-        $(".niceCheck").each(
-        function() {
+    $(".niceCheck").each(
+        function () {
             changeCheckStart($(this));
         });
 
     function changeCheck(el) {
         var el = el,
-        input = el.find("input").eq(0);
-        if(!input.attr("checked")) {
-            el.css("background-position","0 -17px");
+            input = el.find("input").eq(0);
+        if (!input.attr("checked")) {
+            el.css("background-position", "0 -17px");
             input.attr("checked", true)
         } else {
-            el.css("background-position","0 0");
+            el.css("background-position", "0 0");
             input.attr("checked", false)
         }
         return true;
     }
 
-    function changeCheckStart(el){
+    function changeCheckStart(el) {
         var el = el,
-        input = el.find("input").eq(0);
-        if(input.attr("checked")) {
-            el.css("background-position","0 -17px");
+            input = el.find("input").eq(0);
+        if (input.attr("checked")) {
+            el.css("background-position", "0 -17px");
         }
         return true;
     }
+
+    /*jQuery(document).ready(function () {
+        jQuery(".main_item").mouseenter(
+            function () {
+                jQuery(this).animate({
+                    backgroundColor: "#03C",
+                }, 500);
+
+            });
+        jQuery(".main_item").mouseleave(function () {
+            jQuery(this).animate({
+                backgroundColor: "#0CF",
+            }, 500);
+        });
+    });*/
 
 });
