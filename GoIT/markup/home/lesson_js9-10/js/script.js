ca6aff0b51ac14047f49ba97f628aa805bd18a47
ivan.datsiv <ivan_datsiv@mail.ru>
2016-04-15 20:03:00 +0300

    lesson_js9-10 cont...

--- GoIT/markup/home/lesson_js9-10/js/script.js
+++ GoIT/markup/home/lesson_js9-10/js/script.js
@@ -3,11 +3,11 @@
  */
 
 $(function () {
-    /*$("#default-usage-select").selectbox().bind('change', function () {
+    $("#default-usage-select").selectbox().bind('change', function () {
         $('<div>Value of #default-usage-select changed to: ' + $(this).val() + '</div>').appendTo('#demo-default-usage .demoTarget').fadeOut(5000, function () {
             $(this).remove();
         });
-    });*/
+    });
 
     $(".niceCheck").mousedown(
         function () {
