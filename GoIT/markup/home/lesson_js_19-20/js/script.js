28cd577cc5b7ecec381421dc5562da0e1c541ecf
ivan.datsiv <ivan_datsiv@mail.ru>
2016-05-11 19:08:50 +0300

    lesson_js_19-20 first part.

---
+++ GoIT/markup/home/lesson_js_19-20/js/script.js
@@ -0,0 +1,8 @@
+/**
+ * Created by ivan.datsiv on 5/11/2016.
+ */
+$(function() {
+    $('.jcarousel').jcarousel({
+        // Configuration goes here
+    });
+});
--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,7 @@
 /**
  * Created by ivan.datsiv on 4/28/2016.
  */
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     grunt.initConfig({
         concat: {
@@ -9,22 +9,40 @@ module.exports = function(grunt) {
                 separator: ';'
             },
             dist: {
-                src: ['GoIT/markup/home/lesson_js_17-18/js/src/*.js'],
+                src: ['GoIT/markup/home/lesson_js_17-18/js/src1/*.js'],
                 dest: 'GoIT/markup/home/lesson_js_17-18/js/dist/script.main.js'
             }
         },
 
         uglify: {
             dist: {
-                src:['GoIT/markup/home/lesson_js_17-18/js/dist/script.main.js'],
+                src: ['GoIT/markup/home/lesson_js_17-18/js/dist/script.main.js'],
                 dest: 'GoIT/markup/home/lesson_js_17-18/js/dist/script.main.min.js'
+            }
+        },
+
+        sass: {
+            dist: {
+                files: {
+                    //just one file
+                    'GoIT/markup/home/lesson_js_19-20/css/main.css' : 'GoIT/markup/home/lesson_js_19-20/css/style.scss'
+                }
+            }
+        },
+
+        watch: {
+            sass: {
+                files: ['GoIT/markup/home/lesson_js_19-20/css/style.scss'],
+                tasks: ['sass']
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['concat', 'uglify']);
+    grunt.registerTask('default', ['concat', 'uglify', 'sass']);
 
 };
