2544efe26e565b2c64e04f376bf1455318cc0f0b
ivan.datsiv <ivan_datsiv@mail.ru>
2016-05-17 18:33:43 +0300

    lesson_js_19-20 first part4.

--- GoIT/markup/home/lesson_js_19-20/js/script.js
+++ GoIT/markup/home/lesson_js_19-20/js/script.js
@@ -5,4 +5,14 @@ $(function() {
     $('.jcarousel').jcarousel({
         // Configuration goes here
     });
+
+    var $h4 = $('.banners_place > h4');
+
+    $h4.on('click', click_banners);
+
+    function click_banners (){
+        $(this).next().toggle();
+        $(this).toggleClass('h4_click');
+        /*$(this).before("").css('background-color','white');*/
+    }
 });
